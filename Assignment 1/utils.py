af637f2d96cd0c5d5144bad362c3d02d131a1145
Yasmine Ghanem <74925701+yasmineghanem@users.noreply.github.com>
2024-03-31 01:04:11 +0200

    commit

--- Assignment 1/utils.py
+++ Assignment 1/utils.py
@@ -1,1 +1,8 @@
 # includes all utility functions for the assignment
+
+class NFAState:
+    def __init__(self, state_name):
+        self.state_name = state_name # the name of the state
+        self.transitions = {} # possible transitions from this state
+        self.epsilon_transitions = [] # possible epsilon transitions from this state
+        self.is_accepting = False # is this state an accepting state
